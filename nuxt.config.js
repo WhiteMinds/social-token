26b9ee2b9edf6cf5c6fce9f2ae2aad9894c66240
WhiteMind <WhiteMind@qq.com>
2023-07-14 17:18:37 +0800

    tmp: yarn add up-ckb-alpha-test

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,8 +88,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    // analyze: process.env.NODE_ENV === 'production',
-    transpile: [/^element-ui/],
+    analyze: process.env.NODE_ENV === 'production',
+    transpile: [/^element-ui/, /up-ckb-alpha-test/],
     // publicPath: ,
     optimization: {
       splitChunks: {
