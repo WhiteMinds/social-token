9de59ad45fd4014de56c4a75fc3b30b4e1ac95ec
whitemind <whitemind@qq.com>
2023-07-26 17:11:48 +0800

    feat: install packages up-core-test and up-ckb-alpha-test

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     // analyze: process.env.NODE_ENV === 'production',
-    transpile: [/^element-ui/],
+    transpile: [/^element-ui/, /up-ckb-alpha-test/],
     // publicPath: ,
     optimization: {
       splitChunks: {
